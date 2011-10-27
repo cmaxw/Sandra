b14e77e9fd1feccf51afc18774b523a7e8dda4b1
Charles Max Wood <chuck@teachmetocode.com>
2011-10-27 15:43:41 -0600

    add activemodel callbacks for save, update, destroy, and create

--- lib/sandra.rb
+++ lib/sandra.rb
@@ -6,9 +6,11 @@ module Sandra
   def self.included(base)
     base.extend(ClassMethods)
     base.extend(ActiveModel::Naming)
+    base.extend(ActiveModel::Callbacks)
     base.class_eval do
       include ActiveModel::Validations
       include ActiveModel::Conversion
+      define_model_callbacks :create, :update, :save, :destroy
       attr_accessor :attributes, :new_record
       def initialize(attrs = {})
         @attributes = attrs.stringify_keys
@@ -26,19 +28,24 @@ module Sandra
   end
 
   def save
-    if valid?
-      attrs = attributes.dup
-      key = attrs.delete(self.class.key)
-      self.class.insert(key, attrs)
-      new_record = false
-      true
-    else
-      false
+    callback_target = self.new_record? ? :create : :update
+    run_callbacks callback_target do
+      run_callbacks :save do
+        attrs = attributes.dup
+        key = attrs.delete(self.class.key)
+        if key && valid?
+          self.class.insert(key, attrs)
+          new_record = false
+          true
+        else
+          false
+        end
+      end
     end
   end
 
   module ClassMethods
-    def column(col_name)
+    def column(col_name, type)
       define_method col_name do
         attr = col_name.to_s
         attributes[attr]
@@ -50,7 +57,7 @@ module Sandra
     end
 
     def establish_connection(options = {})
-      connection_options = YAML.load_file("#{::Rails.root.to_s}/config/sandra.yml")[Rails.env].merge(options)
+      connection_options = YAML.load_file("#{::Rails.root.to_s}/config/sandra.yml")[::Rails.env].merge(options)
       keyspace = connection_options["keyspace"]
       host = "#{connection_options["host"]}:#{connection_options["port"]}"
       @connection = Cassandra.new(keyspace, host)
@@ -61,25 +68,29 @@ module Sandra
     end
 
     def get(key)
-      hash = connection.get("User", key)
+      hash = connection.get(self.to_s, key)
       unless hash.empty?
-        obj = self.new(hash)
-        obj.username = key
-        obj.new_record = false
-        obj
+        self.new_object(key, hash)
       else
         nil
       end
     end
 
-    def insert(key, columns = {})
-      connection.insert("User", key, columns)
+    def new_object(key, attributes)
+      obj = self.new(attributes)
+      obj.send("#{@key}=", key)
+      obj.new_record = false
+      obj
     end
 
-    def key_attribute(name)
+    def insert(key, columns = {})
+      connection.insert(self.to_s, key, columns)
+    end
+
+    def key_attribute(name, type)
       @key = name
       validates name, :presence => true, :key => true
-      column name
+      column name, type
     end
 
     def key
@@ -90,6 +101,12 @@ module Sandra
       obj = self.new(columns)
       obj.save
       obj
+    end
+
+    def range(options)
+      connection.get_range(self.to_s, options).map do |key, value|
+        self.new_object(key, value)
+      end
     end
   end
 end
--- lib/sandra/key_validator.rb
+++ lib/sandra/key_validator.rb
@@ -1,7 +1,11 @@
 module Sandra
   class KeyValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      record.errors[attribute] << "#{record.attributes[attribute.to_s]} has been taken." if record.new_record? && record.class.get(record.attributes[attribute.to_s])
+      if record.attributes[attribute.to_s].nil?
+        record.errors[attribute] << "#{attribute.to_s} cannot be nil."
+      else
+        record.errors[attribute] << "#{record.attributes[attribute.to_s]} has been taken." if record.new_record? && record.class.get(record.attributes[attribute.to_s])
+      end
     end
   end
 end
